541579a8f8f88d41271c465c012a34d59811ac66
Ahmed <ahmedsaidmeod@gmail.com>
2026-08-11 14:09:13 +0300

    feat: implement lifespan context manager for FastAPI with MongoDB connection

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from pymongo import AsyncMongoClient
 from routes import base, data
+from helpers.config import get_settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code here
+    settings = get_settings()
+
+    app.mongo_conn = AsyncMongoClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    yield
+
+    # Shutdown code here
+    await app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
