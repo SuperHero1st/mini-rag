54c953d162e4491f218c69b1d4e1c404efac16a9
Ahmed <ahmedsaidmeod@gmail.com>
2026-08-11 14:57:03 +0300

    feat: enhance upload_data endpoint to include project_id in response and add request dependency

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status, Request
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
@@ -7,6 +7,7 @@ import aiofiles
 from models import ResponseSignal
 import logging
 from .schemas.data import ProcessRequest
+from models.ProjectModel import ProjectModel
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -16,10 +17,17 @@ data_router = APIRouter(
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data(project_id: str, file: UploadFile,
+async def upload_data(request: Request, project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
         
-    
+    project_model = ProjectModel(
+        db_client=request.app.db_client
+    )
+
+    project = project_model.get_project_or_create_one(
+        project_id=project_id
+    )
+
     # validate the file properties
     data_controller = DataController()
 
@@ -57,7 +65,8 @@ async def upload_data(project_id: str, file: UploadFile,
     return JSONResponse(
             content={
                 "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
+                "file_id": file_id,
+                "project_id": str(project._id)
             }
         )
 
